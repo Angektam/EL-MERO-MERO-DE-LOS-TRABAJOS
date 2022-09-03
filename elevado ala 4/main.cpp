2333ff76df57e00c4cf9ee18eccb61d244153c44
Angektam <75699186+Angektam@users.noreply.github.com>
2022-09-02 21:41:02 -0600

    nada de nada

---
+++ elevado ala 4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    double numero =5;
+    double elevado = pow(numero, 2);
+    cout << "El numero " << numero << " elevado al cuadrado es " << elevado;
+    return 0;
+}
---
+++ suma de numeros/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  
+    cout << 3+4<< endl;
+    cout << 3-4*4;
+    return 0;
+}
